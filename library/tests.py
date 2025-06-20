2f30ff0cef4cb3f13e5cf50f60751e799b2e9df3
pacable <igor.mamaev1@gmail.com>
2025-06-20 16:44:15 +0300

    refactored tests for Book api so they run 14000 times faster

--- library/tests.py
+++ library/tests.py
@@ -7,22 +7,24 @@ from rest_framework import status
 from library.models import Book
 
 
-class BaseTestCase(APITestCase):
-    def setUp(self):
+class BaseBookTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
         # Create normal user
-        self.user = User.objects.create_user(
+        cls.user = User.objects.create_user(
             username='testuser',
             password='testpass123',
         )
 
         # Create admin user
-        self.admin_user = User.objects.create_user(
+        cls.admin_user = User.objects.create_user(
             username='admin',
             password='admin123',
             email='admin@example.com',
             is_staff=True
         )
 
+    def setUp(self):
         self.sample_book = {
             'title': 'Test Book',
             'author': 'Test Author',
@@ -30,23 +32,160 @@ class BaseTestCase(APITestCase):
             'available_copies': 5
         }
         self.book = Book.objects.create(**self.sample_book)
+        self.authenticate()
 
-    def authenticate_user(self):
+    def authenticate(self):
         """
-        Helper function that authenticates as a normal user.
+        Helper function that authenticates as a self.user.
         :return:
         """
+        if not hasattr(self, 'user'):
+            return
         self.client.force_authenticate(user=self.user)
 
-    def authenticate_admin(self):
-        """
-        Helper function that authenticates as an admin user.
-        :return:
-        """
-        self.client.force_authenticate(user=self.admin_user)
+    def _add_new_book(self):
+        url = reverse('books view')
+
+        new_book_data = {
+            'title': 'New Book',
+            'author': 'New Author',
+            'isbn': '9876543210123',
+            'available_copies': 3
+        }
+
+        response = self.client.post(url, new_book_data)
+        return response, new_book_data
 
 
-class BookAPITest(BaseTestCase):
+class AdminBookAPIBookTest(BaseBookTestCase):
+    def setUp(self):
+        self.user = self.admin_user  # Use the admin user for these tests
+        super().setUp()
+
+    def test_add_new_book_success(self):
+        response, new_book_data = self._add_new_book()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'message': 'ok'})
+        self.assertTrue(Book.objects.filter(isbn=new_book_data['isbn']).exists())
+
+    def test_add_existing_book_increases_copies(self):
+        url = reverse('books view')
+
+        additional_copies = {
+            'title': self.sample_book['title'],
+            'author': self.sample_book['author'],
+            'isbn': self.sample_book['isbn'],
+            'available_copies': 3
+        }
+
+        initial_copies = self.book.available_copies
+        response = self.client.post(url, additional_copies)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.available_copies, initial_copies + additional_copies['available_copies'])
+
+    def test_add_book_missing_title(self):
+        url = reverse('books view')
+
+        invalid_book = {
+            'author': 'Test Author',
+            'isbn': '1234567890123',
+            'available_copies': 5
+        }
+
+        response = self.client.post(url, invalid_book)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Please provide book title'})
+
+    def test_add_book_missing_author(self):
+        url = reverse('books view')
+
+        invalid_book = {
+            'title': 'Test Book',
+            'isbn': '1234567890123',
+            'available_copies': 5
+        }
+
+        response = self.client.post(url, invalid_book)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Please provide book author'})
+
+    def test_add_book_missing_isbn(self):
+        url = reverse('books view')
+
+        invalid_book = {
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'available_copies': 5
+        }
+
+        response = self.client.post(url, invalid_book)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Please provide book isbn'})
+
+    def test_add_book_invalid_copies(self):
+        url = reverse('books view')
+
+        invalid_book = {
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'isbn': '1234567890123',
+            'available_copies': 'invalid'
+        }
+
+        response = self.client.post(url, invalid_book)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('message', response.data)
+
+    def test_add_book_zero_copies(self):
+        url = reverse('books view')
+
+        invalid_book = {
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'isbn': '1234567890123',
+            'available_copies': 0
+        }
+
+        response = self.client.post(url, invalid_book)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Please provide available copies'})
+
+
+class UserBookAPIBookTest(BaseBookTestCase):
+    def setUp(self):
+        super().setUp()
+        # The default user from BaseBookTestCase will be used
+
+    def test_add_new_book_wrong_permissions(self):
+        response, new_book_data = self._add_new_book()
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertFalse(Book.objects.filter(isbn=new_book_data['isbn']).exists())
+
+    def test_forbidden_method(self):
+        url = reverse('books view')
+
+        new_book_data = {
+            'title': 'New Book',
+            'author': 'New Author',
+            'isbn': '9876543210123',
+            'available_copies': 3
+        }
+
+        response = self.client.put(url, new_book_data)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class BookAPIBookTest(BaseBookTestCase):
     def test_list_books_empty(self):
         # Delete the book created in setUp
         Book.objects.all().delete()
@@ -64,145 +203,3 @@ class BookAPITest(BaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['books']), 1)
         self.assertIn(str(self.book), response.data['books'])
-
-    def test_add_new_book_success(self):
-        self.authenticate_admin()
-        response, new_book_data = self._add_new_book()
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'message': 'ok'})
-        self.assertTrue(Book.objects.filter(isbn=new_book_data['isbn']).exists())
-
-    def test_add_new_book_wrong_permissions(self):
-        self.authenticate_user()
-        response, new_book_data = self._add_new_book()
-
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertFalse(Book.objects.filter(isbn=new_book_data['isbn']).exists())
-
-    def _add_new_book(self):
-        url = reverse('books view')
-
-        new_book_data = {
-            'title': 'New Book',
-            'author': 'New Author',
-            'isbn': '9876543210123',
-            'available_copies': 3
-        }
-        
-        response = self.client.post(url, new_book_data)
-        return response, new_book_data
-
-    def test_add_existing_book_increases_copies(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        additional_copies = {
-            'title': self.sample_book['title'],
-            'author': self.sample_book['author'],
-            'isbn': self.sample_book['isbn'],
-            'available_copies': 3
-        }
-        
-        initial_copies = self.book.available_copies
-        response = self.client.post(url, additional_copies)
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.book.refresh_from_db()
-        self.assertEqual(self.book.available_copies, initial_copies + additional_copies['available_copies'])
-
-    def test_add_book_missing_title(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        invalid_book = {
-            'author': 'Test Author',
-            'isbn': '1234567890123',
-            'available_copies': 5
-        }
-        
-        response = self.client.post(url, invalid_book)
-        
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {'message': 'Please provide book title'})
-
-    def test_add_book_missing_author(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        invalid_book = {
-            'title': 'Test Book',
-            'isbn': '1234567890123',
-            'available_copies': 5
-        }
-        
-        response = self.client.post(url, invalid_book)
-        
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {'message': 'Please provide book author'})
-
-    def test_add_book_missing_isbn(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        invalid_book = {
-            'title': 'Test Book',
-            'author': 'Test Author',
-            'available_copies': 5
-        }
-        
-        response = self.client.post(url, invalid_book)
-        
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {'message': 'Please provide book isbn'})
-
-    def test_add_book_invalid_copies(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        invalid_book = {
-            'title': 'Test Book',
-            'author': 'Test Author',
-            'isbn': '1234567890123',
-            'available_copies': 'invalid'
-        }
-        
-        response = self.client.post(url, invalid_book)
-        
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('message', response.data)
-
-    def test_add_book_zero_copies(self):
-        self.authenticate_admin()
-        url = reverse('books view')
-
-        invalid_book = {
-            'title': 'Test Book',
-            'author': 'Test Author',
-            'isbn': '1234567890123',
-            'available_copies': 0
-        }
-        
-        response = self.client.post(url, invalid_book)
-        
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, {'message': 'Please provide available copies'})
-
-    def test_forbidden_method(self):
-        self.authenticate_user()
-        url = reverse('books view')
-
-        new_book_data = {
-            'title': 'New Book',
-            'author': 'New Author',
-            'isbn': '9876543210123',
-            'available_copies': 3
-        }
-
-        response = self.client.put(url, new_book_data)
-
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
-class BorrowAPITest(BaseTestCase):
-    ...
