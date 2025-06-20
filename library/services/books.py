37972a2d3c45a91db7e90f99ad0c90993e9f56b5
pacable <77161122+pxc1984@users.noreply.github.com>
2025-06-20 16:47:26 +0300

    refactor book logic (#2)
    * refactor book logic
    
    * Refactored views.py
    
    * Added tests
    
    * 100% coverage
    
    * refactored tests for Book api so they run 14000 times faster

---
+++ library/services/books.py
@@ -0,0 +1,16 @@
+﻿from library.models import Book
+
+
+def list_books() -> list[str]:
+    book_queryset = Book.objects.all()
+    resp = [str(book) for book in book_queryset]
+    return resp
+
+def add_or_increase_book(book_info: dict[str, str | int]):
+    query = Book.objects.filter(isbn=book_info['isbn'])
+    if not query.exists():
+        obj = Book.objects.create(**book_info)
+    else:
+        obj = query.first()
+        obj.available_copies += book_info['available_copies']
+    obj.save()
--- library/views.py
+++ library/views.py
@@ -1,71 +1,83 @@
-import enum
+from library.services.books import list_books, add_or_increase_book
 
-from django.core.handlers.wsgi import WSGIRequest
+from dataclasses import dataclass
+from typing import Optional
 from django.http import HttpRequest
-from jwt import ExpiredSignatureError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN, HTTP_405_METHOD_NOT_ALLOWED
 
-from library.models import Book
+
+@dataclass
+class BookData:
+    title: str
+    author: str
+    isbn: str
+    available_copies: int
+
+
+class BookValidator:
+    ERROR_MESSAGES = {
+        'missing_title': "Please provide book title",
+        'missing_author': "Please provide book author",
+        'missing_isbn': "Please provide book isbn",
+        'missing_copies': "Please provide available copies"
+    }
+
+    @staticmethod
+    def validate_request(request: HttpRequest) -> tuple[Optional[BookData], Optional[str]]:
+        title = request.POST.get('title')
+        if not title:
+            return None, BookValidator.ERROR_MESSAGES['missing_title']
+
+        author = request.POST.get('author')
+        if not author:
+            return None, BookValidator.ERROR_MESSAGES['missing_author']
+
+        isbn = request.POST.get('isbn')
+        if not isbn:
+            return None, BookValidator.ERROR_MESSAGES['missing_isbn']
+
+        try:
+            available_copies = int(request.POST.get('available_copies'))
+            if not available_copies:
+                return None, BookValidator.ERROR_MESSAGES['missing_copies']
+        except ValueError as e:
+            return None, str(e)
+
+        return BookData(
+            title=title,
+            author=author,
+            isbn=isbn,
+            available_copies=available_copies
+        ), None
 
 
 @api_view(['GET', 'POST'])
-def books_view(request):
+def books_view(request: HttpRequest) -> Response:
     if request.method == 'GET':
-        return _list_books_view(request)
-    elif request.method == 'POST':
-        return _add_new_book_view(request)
-    else:
-        return Response({'message': 'forbidden'}, status=HTTP_403_FORBIDDEN)
+        return create_books_list_response()
+    else: # elif request.method == 'POST':
+        if not request.user.is_staff:
+            return Response({'message': 'Admin privileges required'}, status=HTTP_403_FORBIDDEN)
+        return handle_book_creation(request)
 
 
-def _list_books_view(request):
-    book_queryset = Book.objects.all()
-    resp = {'books': []}
-    for book in book_queryset:
-        resp['books'].append(str(book))
-    return Response(resp, status=HTTP_200_OK)
+def create_books_list_response() -> Response:
+    return Response({'books': list_books()}, status=HTTP_200_OK)
 
 
-def _add_new_book_view(request):
-    # TODO: add checking for admin permissions
+def handle_book_creation(request: HttpRequest) -> Response:
+    book_data, err = BookValidator.validate_request(request)
 
-    book_info: dict[str, str | int]
-    err_text: str
-    book_info, err_text = _parse_book(request)
-    if err_text is not None:
-        return Response({'message': err_text}, status=HTTP_400_BAD_REQUEST)
+    if err:
+        return Response({'message': err}, status=HTTP_400_BAD_REQUEST)
 
-    query = Book.objects.filter(isbn=book_info['isbn'])
-    if not query.exists():
-        obj = Book.objects.create(**book_info)
-    else:
-        obj = query.first()
-        obj.available_copies += book_info['available_copies']
-    obj.save()
-
+    book_info = {
+        'title': book_data.title,
+        'author': book_data.author,
+        'isbn': book_data.isbn,
+        'available_copies': book_data.available_copies
+    }
+    add_or_increase_book(book_info)
     return Response({'message': 'ok'}, status=HTTP_200_OK)
-
-
-def _parse_book(request: WSGIRequest) -> tuple[dict[str, str | int], None] | tuple[None, str]:
-    title = request.POST.get('title')
-    if not title:
-        return None, "Please provide book title"
-
-    author = request.POST.get('author')
-    if not author:
-        return None, "Please provide book author"
-
-    isbn = request.POST.get('isbn')
-    if not isbn:
-        return None, "Please provide book isbn"
-
-    try:
-        available_copies = int(request.POST.get('available_copies'))
-        if not available_copies:
-            return None, "Please provide available copies"
-    except ValueError as e:
-        return None, str(e)
-
-    return {'title': title, 'author': author, 'isbn': isbn, 'available_copies': available_copies}, None
